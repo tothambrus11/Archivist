837897411d17e2e35c1cfad4142783d4bf0158fd
Ambrus Tóth <ping@ambrus.dev>
2025-04-20 18:01:17 +0200

    Fix failing build
    Fix AcrhivistMacros typo and remove extra import that causes build failing

--- Sources/Archivist/Macro.swift
+++ Sources/Archivist/Macro.swift
@@ -1,5 +1,3 @@
-import ArchivistMacros
-
 @attached(member, names: named(init), named(write))
 @attached(extension, conformances: Archivable)
 public macro Archivable() = #externalMacro(module: "ArchivistMacros", type: "ArchivableMacro")
